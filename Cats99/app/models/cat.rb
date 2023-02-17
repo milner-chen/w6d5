ad0a11bc96274f9248ab6962e7a55e53570b39ee
Milner Chen <milnerchen0@gmail.com>
2023-02-17 17:49:07 -0500

    create a cat

---
+++ Cats99/app/models/cat.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'action_view'
+# require 'action_view/helpers'
+include ActionView::Helpers::DateHelper
+
+class Cat < ApplicationRecord
+    # include ActionView::Helpers::DateHelper
+
+    CAT_COLORS = ['gray', 'orange', 'black', 'calico', 'tabby']
+
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :color, inclusion: { in: CAT_COLORS }
+    validates :sex, inclusion: { in: ["M", "F"] }
+    validate :birth_date_cannot_be_in_the_future
+
+    def birth_date_cannot_be_in_the_future
+        if birth_date > Date.today
+            errors.add(:birth_date)
+        end
+    end
+
+    def age
+        time_ago_in_words(birth_date)
+    end
+
+end
